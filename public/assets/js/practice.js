2518f63df9ea4322de0283c386197715d6f7354c
mr-himanshuyadav <85346472+mr-himanshuyadav@users.noreply.github.com>
2025-07-04 20:51:18 +0530

    fix: Restore timer input toggle functionality on start form

--- public/assets/js/practice.js
+++ public/assets/js/practice.js
@@ -2,12 +2,23 @@
 
 jQuery(document).ready(function($) {
 
-    $('#qp-practice-app-wrapper').on('submit', '#qp-start-practice-form', function(e) {
+    var wrapper = $('#qp-practice-app-wrapper');
+
+    // NEW: Logic for the timer checkbox on the settings form
+    wrapper.on('change', '#qp_timer_enabled_cb', function() {
+        if ($(this).is(':checked')) {
+            $('#qp-timer-input-wrapper').slideDown();
+        } else {
+            $('#qp-timer-input-wrapper').slideUp();
+        }
+    });
+
+    // Logic for submitting the settings form to start the practice
+    wrapper.on('submit', '#qp-start-practice-form', function(e) {
         e.preventDefault(); // Stop the form from submitting the traditional way
 
         var formData = $(this).serialize();
-        var wrapper = $('#qp-practice-app-wrapper');
-
+        
         $.ajax({
             url: qp_ajax_object.ajax_url, // URL provided by WordPress
             type: 'POST',
@@ -17,8 +28,7 @@ jQuery(document).ready(function($) {
                 settings: formData
             },
             beforeSend: function() {
-                // Show a loading message
-                wrapper.html('<p>Loading your session...</p>');
+                wrapper.html('<p style="text-align:center; padding: 50px;">Loading your session...</p>');
             },
             success: function(response) {
                 if (response.success) {
