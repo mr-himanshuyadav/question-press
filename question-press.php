0454b6b385e0213237274fa5b0cf1dc7ce4f3e7a
mr-himanshuyadav <85346472+mr-himanshuyadav@users.noreply.github.com>
2025-08-01 10:58:46 +0530

    feat: Update

--- question-press.php
+++ question-press.php
@@ -3,7 +3,7 @@
 /**
  * Plugin Name:       Question Press
  * Description:       A complete plugin for creating, managing, and practicing questions.
- * Version:           3.2.4
+ * Version:           3.3.0
  * Author:            Himanshu
  */
 
