b867c77d32d1ef357315022d99fa8fa3a4895c4e
mr-himanshuyadav <85346472+mr-himanshuyadav@users.noreply.github.com>
2025-10-26 19:09:51 +0530

    Refactor: Move REST auth callbacks to Rest_Api\AuthController

--- api/class-qp-rest-api.php
+++ api/class-qp-rest-api.php
@@ -6,6 +6,7 @@ require_once QP_PLUGIN_PATH . 'lib/JWT.php';
 require_once QP_PLUGIN_PATH . 'lib/Key.php';
 use Firebase\JWT\JWT;
 use Firebase\JWT\Key;
+use QuestionPress\Rest_Api\AuthController;
 
 class QP_Rest_Api {
 
@@ -23,7 +24,7 @@ class QP_Rest_Api {
         // --- Authentication Endpoint (Public) ---
         register_rest_route('questionpress/v1', '/token', [
             'methods' => WP_REST_Server::CREATABLE, // This is equivalent to POST
-            'callback' => [self::class, 'get_auth_token'],
+            'callback' => [AuthController::class, 'get_auth_token'], // CHANGED
             'permission_callback' => '__return_true'
         ]);
 
@@ -31,147 +32,82 @@ class QP_Rest_Api {
         register_rest_route('questionpress/v1', '/subjects', [
             'methods' => WP_REST_Server::READABLE, // This is equivalent to GET
             'callback' => [self::class, 'get_subjects'],
-            'permission_callback' => [self::class, 'check_auth_token']
+            'permission_callback' => [AuthController::class, 'check_auth_token'] // CHANGED
         ]);
 
         // --- Topics Endpoint (Protected) ---
         register_rest_route('questionpress/v1', '/topics', [
             'methods' => WP_REST_Server::READABLE, // GET
             'callback' => [self::class, 'get_topics'],
-            'permission_callback' => [self::class, 'check_auth_token']
+            'permission_callback' => [AuthController::class, 'check_auth_token'] // CHANGED
         ]);
 
         // --- Exams Endpoint (Protected) ---
         register_rest_route('questionpress/v1', '/exams', [
             'methods' => WP_REST_Server::READABLE, // GET
             'callback' => [self::class, 'get_exams'],
-            'permission_callback' => [self::class, 'check_auth_token']
+            'permission_callback' => [AuthController::class, 'check_auth_token'] // CHANGED
         ]);
 
         // --- Sources Endpoint (Protected) ---
         register_rest_route('questionpress/v1', '/sources', [
             'methods' => WP_REST_Server::READABLE, // GET
             'callback' => [self::class, 'get_sources'],
-            'permission_callback' => [self::class, 'check_auth_token']
+            'permission_callback' => [AuthController::class, 'check_auth_token'] // CHANGED
         ]);
 
         // --- Labels Endpoint (Protected) ---
         register_rest_route('questionpress/v1', '/labels', [
             'methods' => WP_REST_Server::READABLE, // GET
             'callback' => [self::class, 'get_labels'],
-            'permission_callback' => [self::class, 'check_auth_token']
+            'permission_callback' => [AuthController::class, 'check_auth_token'] // CHANGED
         ]);
 
         // --- Add Question Group Endpoint (Protected) ---
         register_rest_route('questionpress/v1', '/questions/add', [
             'methods' => WP_REST_Server::CREATABLE, // POST
             'callback' => [self::class, 'add_question_group'],
-            'permission_callback' => [self::class, 'check_auth_token']
+            'permission_callback' => [AuthController::class, 'check_auth_token'] // CHANGED
         ]);
         
         // --- Start Session / Get Questions Endpoint (Protected) ---
         register_rest_route('questionpress/v1', '/start-session', [
             'methods' => WP_REST_Server::CREATABLE, // POST
             'callback' => [self::class, 'start_session_and_get_questions'],
-            'permission_callback' => [self::class, 'check_auth_token']
+            'permission_callback' => [AuthController::class, 'check_auth_token'] // CHANGED
         ]);
         
         register_rest_route('questionpress/v1', '/question/(?P<id>\d+)', [
-    'methods' => WP_REST_Server::READABLE, // GET
-    'callback' => [self::class, 'get_single_question_by_id'],
-    'permission_callback' => [self::class, 'check_auth_token'],
-    'args' => [
-        'id' => [
-            'validate_callback' => function($param, $request, $key) {
-                return is_numeric($param);
-            }
-        ],
-    ],
-]);
+            'methods' => WP_REST_Server::READABLE, // GET
+            'callback' => [self::class, 'get_single_question_by_id'],
+            'permission_callback' => [AuthController::class, 'check_auth_token'], // CHANGED
+            'args' => [
+                'id' => [
+                    'validate_callback' => function($param, $request, $key) {
+                        return is_numeric($param);
+                    }
+                ],
+            ],
+        ]);
 
         // --- NEW: Session Management Endpoints (Protected) ---
         register_rest_route('questionpress/v1', '/session/create', [
             'methods' => WP_REST_Server::CREATABLE, // POST
             'callback' => [self::class, 'create_session'],
-            'permission_callback' => [self::class, 'check_auth_token']
+            'permission_callback' => [AuthController::class, 'check_auth_token'] // CHANGED
         ]);
 
         register_rest_route('questionpress/v1', '/session/attempt', [
             'methods' => WP_REST_Server::CREATABLE, // POST
             'callback' => [self::class, 'record_attempt'],
-            'permission_callback' => [self::class, 'check_auth_token']
+            'permission_callback' => [AuthController::class, 'check_auth_token'] // CHANGED
         ]);
 
         register_rest_route('questionpress/v1', '/session/end', [
             'methods' => WP_REST_Server::CREATABLE, // POST
             'callback' => [self::class, 'end_session'],
-            'permission_callback' => [self::class, 'check_auth_token']
+            'permission_callback' => [AuthController::class, 'check_auth_token'] // CHANGED
         ]);
-    }
-
-    /**
-     * Permission callback. Checks for a valid JWT in the Authorization header.
-     */
-    public static function check_auth_token(WP_REST_Request $request) {
-        $secret_key = get_option('qp_jwt_secret_key');
-        if (!$secret_key) {
-            return new WP_Error('rest_jwt_not_configured', 'The JWT secret key has not been configured.', ['status' => 500]);
-        }
-
-        $auth_header = $request->get_header('Authorization');
-        if (!$auth_header) {
-            return new WP_Error('rest_forbidden', 'Authentication token not found.', ['status' => 401]);
-        }
-
-        list($token) = sscanf($auth_header, 'Bearer %s');
-        if (!$token) {
-            return new WP_Error('rest_forbidden', 'Malformed authentication token.', ['status' => 401]);
-        }
-
-        try {
-            $decoded = JWT::decode($token, new Key($secret_key, 'HS256'));
-            wp_set_current_user($decoded->data->user->id);
-            return true;
-        } catch (Exception $e) {
-            return new WP_Error('rest_forbidden', $e->getMessage(), ['status' => 401]);
-        }
-    }
-
-    public static function get_auth_token(WP_REST_Request $request) {
-        $username = $request->get_param('username');
-        $password = $request->get_param('password');
-
-        if (empty($username) || empty($password)) {
-            return new WP_Error('rest_invalid_request', 'Username and password are required.', ['status' => 400]);
-        }
-
-        $user = wp_authenticate($username, $password);
-
-        if (is_wp_error($user)) {
-            return new WP_Error('rest_invalid_credentials', 'Invalid username or password.', ['status' => 403]);
-        }
-
-        $secret_key = get_option('qp_jwt_secret_key');
-        if (!$secret_key) {
-            return new WP_Error('rest_jwt_not_configured', 'The JWT secret key has not been configured.', ['status' => 500]);
-        }
-        
-        $issued_at = time();
-        $expire_at = $issued_at + (DAY_IN_SECONDS * 7);
-
-        $payload = [
-            'iss' => get_bloginfo('url'), 'iat' => $issued_at, 'nbf' => $issued_at, 'exp' => $expire_at,
-            'data' => [ 'user' => [ 'id' => $user->ID ] ]
-        ];
-
-        $token = JWT::encode($payload, $secret_key, 'HS256');
-
-        return new WP_REST_Response([
-            'token'             => $token,
-            'user_email'        => $user->user_email,
-            'user_nicename'     => $user->user_nicename,
-            'user_display_name' => $user->display_name
-        ], 200);
     }
 
     public static function get_subjects() {
