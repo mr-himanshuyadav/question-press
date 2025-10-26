154aae345aa7d3cff6efeeff5dd36a5b291eecd2
mr-himanshuyadav <85346472+mr-himanshuyadav@users.noreply.github.com>
2025-10-26 12:58:03 +0530

    Refactor: Template admin "Organize > Labels" tab

--- admin/class-qp-labels-page.php
+++ admin/class-qp-labels-page.php
@@ -83,9 +83,6 @@ class QP_Labels_Page
         }
     }
 
-    /**
-     * Renders the HTML for the Labels tab using the new taxonomy system.
-     */
     public static function render()
     {
         global $wpdb;
@@ -125,97 +122,21 @@ class QP_Labels_Page
         
         $labels = $wpdb->get_results($labels_query);
 
+        // Display session messages before loading the template
         if (isset($_SESSION['qp_admin_message'])) {
             $message = html_entity_decode($_SESSION['qp_admin_message']);
             echo '<div id="message" class="notice notice-' . esc_attr($_SESSION['qp_admin_message_type']) . ' is-dismissible"><p>' . $message . '</p></div>';
             unset($_SESSION['qp_admin_message'], $_SESSION['qp_admin_message_type']);
         }
-?>
-        <div id="col-container" class="wp-clearfix">
-            <div id="col-left">
-                <div class="col-wrap">
-                    <div class="form-wrap">
-                        <h2><?php echo $label_to_edit ? 'Edit Label' : 'Add New Label'; ?></h2>
-                        <form method="post" action="admin.php?page=qp-organization&tab=labels">
-                            <?php wp_nonce_field('qp_add_edit_label_nonce'); ?>
-                            <input type="hidden" name="action" value="<?php echo $label_to_edit ? 'update_label' : 'add_label'; ?>">
-                            <?php if ($label_to_edit) : ?>
-                                <input type="hidden" name="term_id" value="<?php echo esc_attr($label_to_edit->term_id); ?>">
-                            <?php endif; ?>
 
-                            <div class="form-field form-required">
-                                <label for="label-name">Name</label>
-                                <input name="label_name" id="label-name" type="text" value="<?php echo $label_to_edit ? esc_attr($label_to_edit->name) : ''; ?>" size="40" required <?php echo ($label_to_edit && $edit_meta['is_default']) ? 'readonly' : ''; ?>>
-                                <?php if ($label_to_edit && $edit_meta['is_default']): ?>
-                                    <p>Default label names cannot be changed.</p>
-                                <?php endif; ?>
-                            </div>
-
-                            <div class="form-field">
-                                <label for="label-description">Description</label>
-                                <textarea name="label_description" id="label-description" rows="3" cols="40"><?php echo esc_textarea($edit_meta['description']); ?></textarea>
-                            </div>
-
-                            <div class="form-field">
-                                <label for="label-color">Color</label>
-                                <input name="label_color" id="label-color" type="text" value="<?php echo esc_attr($edit_meta['color']); ?>" class="qp-color-picker">
-                            </div>
-
-                            <p class="submit">
-                                <input type="submit" class="button button-primary" value="<?php echo $label_to_edit ? 'Update Label' : 'Add New Label'; ?>">
-                                <?php if ($label_to_edit) : ?>
-                                    <a href="admin.php?page=qp-organization&tab=labels" class="button button-secondary">Cancel Edit</a>
-                                <?php endif; ?>
-                            </p>
-                        </form>
-                    </div>
-                </div>
-            </div>
-            <div id="col-right">
-                <div class="col-wrap">
-                    <table class="wp-list-table widefat fixed striped">
-                        <thead>
-                            <tr>
-                                <th>Name</th>
-                                <th>Description</th>
-                                <th>Actions</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            <?php if (!empty($labels)) : foreach ($labels as $label) : ?>
-                                    <tr>
-                                        <td><span style="padding: 2px 8px; border-radius: 3px; color: #fff; background-color: <?php echo esc_attr($label->color); ?>;"><?php echo esc_html($label->name); ?></span></td>
-                                        <td><?php echo esc_html($label->description); ?></td>
-                                        <td>
-                                            <?php
-                                            $edit_nonce = wp_create_nonce('qp_edit_label_' . $label->term_id);
-                                            $edit_link = sprintf('<a href="?page=qp-organization&tab=labels&action=edit&term_id=%s&_wpnonce=%s">Edit</a>', $label->term_id, $edit_nonce);
-
-                                            if (!$label->is_default) {
-                                                $delete_nonce = wp_create_nonce('qp_delete_label_' . $label->term_id);
-                                                $delete_link = sprintf(
-                                                    '<a href="?page=qp-organization&tab=labels&action=delete&term_id=%s&_wpnonce=%s" style="color:#a00;">Delete</a>',
-                                                    $label->term_id,
-                                                    $delete_nonce
-                                                );
-                                                echo $edit_link . ' | ' . $delete_link;
-                                            } else {
-                                                echo $edit_link;
-                                            }
-                                            ?>
-                                        </td>
-                                    </tr>
-                                <?php endforeach;
-                            else : ?>
-                                <tr class="no-items">
-                                    <td colspan="3">No labels found.</td>
-                                </tr>
-                            <?php endif; ?>
-                        </tbody>
-                    </table>
-                </div>
-            </div>
-        </div>
-<?php
+        // Prepare arguments for the template
+        $args = [
+            'label_to_edit' => $label_to_edit,
+            'edit_meta'     => $edit_meta,
+            'labels'        => $labels,
+        ];
+        
+        // Load and echo the template
+        echo qp_get_template_html( 'organization-tab-labels', 'admin', $args );
     }
 }
