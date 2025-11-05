9834cc22624d29350450a6f15ea3a16fbb93557b
mr-himanshuyadav <85346472+mr-himanshuyadav@users.noreply.github.com>
2025-11-05 15:39:14 +0530

    feat: version update for testing github update.

--- question-press.php
+++ question-press.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name:       Question Press
  * Description:       A complete plugin for creating, managing, and practicing questions.
- * Version:           4.1.6
+ * Version:           4.1.7
  * Author:            Himanshu
  * Text Domain:       question-press
  * Domain Path:       /languages
